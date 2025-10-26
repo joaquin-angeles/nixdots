4f08622950daf8ed9497b30b788315d649ef9f08
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-10-26 09:23:03 +0800

    transparent vim

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -6,11 +6,11 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- Transparent background
--- vim.cmd [[
---   hi Normal guibg=NONE ctermbg=NONE
---   hi NonText guibg=NONE ctermbg=NONE
---   hi BufferLineBufferSelected guibg=NONE ctermbg=NONE
--- ]]
+vim.cmd [[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NonText guibg=NONE ctermbg=NONE
+  hi BufferLineBufferSelected guibg=NONE ctermbg=NONE
+]]
 
 -- Relative line numbers
 vim.o.relativenumber = true
