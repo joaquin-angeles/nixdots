868468e84abd8454b5c8aeed83c3e16567f9c7b1
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-10-26 11:08:44 +0800

    Fixed fonts

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  fonts.fontconfig.fonts = with pkgs; [
-    inter
-    noto-fonts-cjk-sans
-    noto-fonts-emoji-blob-bin
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.meslo-lg
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      inter
+      noto-fonts-cjk-sans
+      noto-fonts-emoji-blob-bin
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.meslo-lg
+    ];
+  };
 }
