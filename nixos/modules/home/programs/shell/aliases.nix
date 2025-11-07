901359b9ca9fb17c8d4823787019292cd3c1cb89
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-11-07 18:34:35 +0800

    Changed the aliases for nvim

--- nixos/modules/home/programs/shell/aliases.nix
+++ nixos/modules/home/programs/shell/aliases.nix
@@ -10,10 +10,10 @@
     ll = "eza -l";
     lla = "eza -la";
     ls = "eza";
+    nv = "nvim";
     "nv-run" = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia";
     pk = "pkill";
     vim = "nvim";
-    v = "nvim";
     "nix-shell" = "nix-shell --run $SHELL";
     yazi = "yzcd";
     yz = "yzcd";
