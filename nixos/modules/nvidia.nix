45dbdfd529ac32fa2e1b24956c700952025ec619
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-11-10 07:48:55 +0800

    Removed finegrained

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -17,7 +17,6 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.finegrained = true;
     open = false;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
