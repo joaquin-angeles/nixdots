6a4e29bbf041589a879179625979404b957c820e
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-11-29 20:48:02 +0800

    yes

--- nixos/modules/home/shell/aliases.nix
+++ nixos/modules/home/shell/aliases.nix
@@ -3,18 +3,12 @@
 {
   programs.zsh.shellAliases = {
     bat = "bat --color=always --theme=base16 --style=plain";
-    c = "clear";
     grep = "rg";
-    k = "kill";
     ll = "eza --icons -lh";
     lla = "eza --icons -lha";
     ls = "eza --icons";
     lsa = "eza --icons -a";
-    nv = "nvim";
-    pk = "pkill";
-    v = "nvim";
     vim = "nvim";
-    "nix-shell" = "nix-shell --run $SHELL";
     yazi = "yz";
     zlj = "zellij";
   };
