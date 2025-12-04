e1962dfe56818ffd23bf74652a066c85d3274f42
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-04 17:14:52 +0800

    Yes

---
+++ nixos/modules/home/dotfiles.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+  home.file.".config/foot".source = ../../../foot;
+  home.file.".config/hypr".source = ../../../hyprland;
+  home.file.".config/mako".source = ../../../mako;
+  home.file.".config/nvim".source = ../../../neovim;
+  home.file.".p10k.zsh".source = ../../../zsh/.p10k.zsh;
+  home.file.".config/rofi".source = ../../../rofi;
+  home.file.".config/Kvantum".source = ../../../kvantum;
+  home.file.".config/swayosd".source = ../../../swayosd;
+  home.file.".config/waybar".source = ../../../waybar;
+  home.file.".config/zellij".source = ../../../zellij;
+  home.file.".zprofile".source = ../../../zsh/.zprofile;
+}
