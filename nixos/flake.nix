9fc15c08b9e58264f68b3f73219ff0a4685b5f74
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-20 11:57:58 +0800

    Changed hostname

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -15,7 +15,7 @@
   # Main integrations
   outputs = inputs@{ self, nixpkgs, unstable, home-manager, ... }: {
     # Imported configurations
-    nixosConfigurations.home = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
       modules = [
         ./configuration.nix # System configuration
 
