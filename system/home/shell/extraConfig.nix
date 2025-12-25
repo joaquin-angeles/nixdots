a0cc6960e004093fed3aa02c922bb2768b039684
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-25 09:01:00 +0800

    Updated system/home/shell/extraConfig.nix

--- system/home/shell/extraConfig.nix
+++ system/home/shell/extraConfig.nix
@@ -1,39 +1,39 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-    programs.zsh.initContent = lib.mkBefore ''
-        # P10K instant prompt
-        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${USER}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${USER}.zsh"
-        fi
-    '';
+    programs.zsh = {
+        initContent = lib.mkMerge [
+            # P10K instant prompt
+            (lib.mkBefore ''
+                # P10K instant prompt
+                if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-${config.home.username}.zsh" ]]; then
+                  source "${config.xdg.cacheHome}/p10k-instant-prompt-${config.home.username}.zsh"
+                fi
+            '')
 
-    programs.zsh.initContent = ''
-        # Fetch
-        if command -v fastfetch >/dev/null 2>&1; then
-            fastfetch
-            echo ""
-        fi
+            # Everything else
+            ''
+                # P10K configuration
+                source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme # Prompt
+                [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-        # P10K configuration
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme # Prompt
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+                # Cursor and title configuration
+                precmd() {
+                    printf '\e[1 q'
+                    print -Pn "\e]0;%n@%m: %~\a"
+                }
 
-        # Cursor and title configuration
-        precmd() {
-            printf '\e[1 q'
-            print -Pn "\e]0;%n@%m: %~\a"
-        }
-
-        # Better FZF (faster preview)
-        function ff() {
-          command fzf --preview '
-            if [ -d {} ]; then
-                eza --icons always --group-directories-first --git --color=always {} || ls -lh {}
-            else
-                bat --color=always {}
-            fi
-          '
-        }
-    '';
+                # Better FZF (faster preview)
+                function ff() {
+                  command fzf --preview '
+                    if [ -d {} ]; then
+                        eza --icons always --group-directories-first --git --color=always {} || ls -lh {}
+                    else
+                        bat --color=always {}
+                    fi
+                  '
+                }
+            ''
+        ];
+    };
 }
