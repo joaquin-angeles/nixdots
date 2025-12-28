8b5885429c248b476beabf69d14fc3a651104022
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-28 09:38:14 +0800

    Fixed up Neovim

--- system/home/dotfiles.nix
+++ system/home/dotfiles.nix
@@ -7,7 +7,7 @@
         "hypr".source = ../../hyprland;
         "Kvantum".source = ../../Qt;
         "mako".source = ../../mako;
-        "nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.files/neovim";
+	"nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.files/neovim";
         "rofi".source = ../../rofi;
         # "swayosd".source = ../../swayosd;
         "waybar".source = ../../waybar;
