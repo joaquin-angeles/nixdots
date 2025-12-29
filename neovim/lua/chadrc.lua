999b2578581e9639287c74da5f12d6a437e057b5
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-29 18:24:52 +0800

    Changed some dashboard stuff

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -19,13 +19,13 @@ M.nvdash = {
 
     -- Modules
     buttons = {
-        { txt = "󰈔  New File", keys = "fn", cmd = "enew" }, -- Make new file
-        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
-        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
-        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
-        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
-        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
-        { txt = "󰍃  Leave / Quit", keys = "qa", cmd = "qa" }, -- Make new file
+        { txt = "󰈔 New File", keys = "fn", cmd = "enew" }, -- Make new file
+        { txt = " Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
+        { txt = " Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
+        { txt = "󰈭 Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
+        { txt = " Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
+        { txt = "󱥚 Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
+        { txt = "󰍃 Leave / Quit", keys = "qa", cmd = "qa" }, -- Make new file
 
         { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
 
