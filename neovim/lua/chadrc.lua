f51b0b5787c52967402f8c1ae989e335e02315eb
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-12-31 08:09:19 +0800

    commit

--- neovim/lua/chadrc.lua
+++ neovim/lua/chadrc.lua
@@ -33,13 +33,13 @@ M.nvdash = {
 
     -- Modules
     buttons = {
-        { txt = "󰈔  New File", keys = "fn", cmd = "enew" }, -- Make new file
-        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
-        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
-        { txt = "󰈭  Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
-        { txt = "  Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
-        { txt = "󱥚  Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
-        { txt = "󰍃  Leave / Quit", keys = "qa", cmd = "qa" }, -- Make new file
+        { txt = "󰈔 New File", keys = "fn", cmd = "enew" }, -- Make new file
+        { txt = " Find File", keys = "ff", cmd = "Telescope find_files" }, -- Fuzzy file search
+        { txt = " Recent Files", keys = "fo", cmd = "Telescope oldfiles" }, -- Recent file search (fuzzy)
+        { txt = "󰈭 Find Word", keys = "fw", cmd = "Telescope live_grep" }, -- Word search (fuzzy)
+        { txt = " Mappings", keys = "ch", cmd = "NvCheatsheet" }, -- Keybind cheatsheet
+        { txt = "󱥚 Themes", keys = "th", cmd = ":lua require('nvchad.themes').open()" }, -- Theme picker
+        { txt = "󰍃 Quit", keys = "q", cmd = "qa!" }, -- Make new file
 
         { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
 
@@ -47,7 +47,7 @@ M.nvdash = {
             txt = function()
                 local stats = require("lazy").stats()
                 local ms = math.floor(stats.startuptime) .. " ms"
-                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+                return " Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
             end,
             hl = "NvDashFooter",
             no_gap = true,
--- yazi/init.lua
+++ yazi/init.lua
@@ -1,140 +1,138 @@
 require("chmod")
-require("full-border"):setup({
-  type = ui.Border.PLAIN,
-})
+require("full-border"):setup({})
 require("smart-enter")
 require("yatline"):setup({
-  --theme = my_theme,
-  section_separator = { open = "", close = "" },
-  part_separator = { open = "", close = "" },
-  inverse_separator = { open = "", close = "" },
+    --theme = my_theme,
+    section_separator = { open = "", close = "" },
+    part_separator = { open = "", close = "" },
+    inverse_separator = { open = "", close = "" },
 
-  style_a = {
-    fg = "green",
-    bg_mode = {
-      normal = nil,
-      select = nil,
-      un_set = nil
+    style_a = {
+        fg = "green",
+        bg_mode = {
+            normal = nil,
+            select = nil,
+            un_set = nil
+        },
     },
-  },
-  style_b = {
-    fg = "grey",
-    bg_mode = {
-      normal = nil,
-      select = nil,
-      un_set = nil
-    }
-  },
-  style_c = {
-    fg = "cyan",
-    bg_mode = {
-      normal = nil,
-      select = nil,
-      un_set = nil
-    }
-  },
-
-  permissions_t_fg = "green",
-  permissions_r_fg = "yellow",
-  permissions_w_fg = "red",
-  permissions_x_fg = "cyan",
-  permissions_s_fg = "grey",
-
-  tab_width = 20,
-  tab_use_inverse = false,
-
-  selected = { icon = "󰻭", fg = "yellow" },
-  copied = { icon = "", fg = "green" },
-  cut = { icon = "", fg = "red" },
-
-  total = { icon = "󰮍", fg = "yellow" },
-  succ = { icon = "", fg = "green" },
-  fail = { icon = "", fg = "red" },
-  found = { icon = "󰮕", fg = "blue" },
-  processed = { icon = "󰐍", fg = "green" },
-
-  show_background = false,
-
-  display_header_line = true,
-  display_status_line = true,
-
-  header_line = {
-    left = {
-      section_a = {
-        {type = "coloreds", custom = true, name = {{"user@nixos-btw", "green"}, {":", "white"}}},
-      },
-      section_b = {
-      },
-      section_c = {
-        {type = "string", custom = false, name = "hovered_path"},
-      }
+    style_b = {
+        fg = "grey",
+        bg_mode = {
+            normal = nil,
+            select = nil,
+            un_set = nil
+        }
     },
-    right = {
-      section_a = {
-        {type = "coloreds", custom = false, name = "count"},
-      },
-      section_b = {
-      },
-      section_c = {
-      }
-    }
-  },
-
-  status_line = {
-    left = {
-      section_a = {
-        {type = "string", custom = false, name = "tab_mode"},
-        {type = "coloreds", custom = false, name = "permissions"},
-      },
-      section_b = {
-        {type = "string", custom = false, name = "hovered_ownership"},
-        {type = "string", custom = false, name = "hovered_size"},
-      },
-      section_c = {
-        {type = "string", custom = false, name = "hovered_file_extension"},
-      }
+    style_c = {
+        fg = "cyan",
+        bg_mode = {
+            normal = nil,
+            select = nil,
+            un_set = nil
+        }
     },
-    right = {
-      section_a = {
-      },
-      section_b = {
-        {type = "string", custom = false, name = "cursor_position"},
-      },
-      section_c = {
-      }
-    }
-  },
+
+    permissions_t_fg = "green",
+    permissions_r_fg = "yellow",
+    permissions_w_fg = "red",
+    permissions_x_fg = "cyan",
+    permissions_s_fg = "grey",
+
+    tab_width = 20,
+    tab_use_inverse = false,
+
+    selected = { icon = "󰻭", fg = "yellow" },
+    copied = { icon = "", fg = "green" },
+    cut = { icon = "", fg = "red" },
+
+    total = { icon = "󰮍", fg = "yellow" },
+    succ = { icon = "", fg = "green" },
+    fail = { icon = "", fg = "red" },
+    found = { icon = "󰮕", fg = "blue" },
+    processed = { icon = "󰐍", fg = "green" },
+
+    show_background = false,
+
+    display_header_line = true,
+    display_status_line = true,
+
+    header_line = {
+        left = {
+            section_a = {
+                {type = "coloreds", custom = true, name = {{"user@nixos-btw", "green"}, {":", "white"}}},
+            },
+            section_b = {
+            },
+            section_c = {
+                {type = "string", custom = false, name = "hovered_path"},
+            }
+        },
+        right = {
+            section_a = {
+                {type = "coloreds", custom = false, name = "count"},
+            },
+            section_b = {
+            },
+            section_c = {
+            }
+        }
+    },
+
+    status_line = {
+        left = {
+            section_a = {
+                {type = "string", custom = false, name = "tab_mode"},
+                {type = "coloreds", custom = false, name = "permissions"},
+            },
+            section_b = {
+                {type = "string", custom = false, name = "hovered_ownership"},
+                {type = "string", custom = false, name = "hovered_size"},
+            },
+            section_c = {
+                {type = "string", custom = false, name = "hovered_file_extension"},
+            }
+        },
+        right = {
+            section_a = {
+            },
+            section_b = {
+                {type = "string", custom = false, name = "cursor_position"},
+            },
+            section_c = {
+            }
+        }
+    },
 })
 
 -- Show user and hostname in top bar
 function Header:host()
-  if ya.target_family() ~= "unix" then
-    return ui.Line {}
-  end
-  return ui.Line { ui.Span(ya.user_name() .. "@" .. ya.host_name()):fg("Lightgreen"):bold(true), ui.Span(":") }
+    if ya.target_family() ~= "unix" then
+        return ui.Line {}
+    end
+    return ui.Line { ui.Span(ya.user_name() .. "@" .. ya.host_name()):fg("Lightgreen"):bold(true), ui.Span(":") }
 end
 
 -- Remove filesize from status bar
 function Status:render(area)
-  self.area = area
-  local left = ui.Line { self:permissions(), self:name() }
-  local right = ui.Line { self:position() }
-  return {
-    ui.Paragraph(area, { left }),
-    ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-    table.unpack(Progress:render(area, right:width())),
-  }
+    self.area = area
+    local left = ui.Line { self:permissions(), self:name() }
+    local right = ui.Line { self:position() }
+    return {
+        ui.Paragraph(area, { left }),
+        ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+        table.unpack(Progress:render(area, right:width())),
+    }
 end
 
 -- Symlink in status bar
 function Status:name()
-  local h = cx.active.current.hovered
-  if not h then
-    return ui.Span("")
-  end
-  local linked = ""
-  if h.link_to ~= nil then
-    linked = " -> " .. tostring(h.link_to)
-  end
-  return ui.Span(" " .. h.name .. linked)
+    local h = cx.active.current.hovered
+    if not h then
+        return ui.Span("")
+    end
+    local linked = ""
+    if h.link_to ~= nil then
+        linked = " -> " .. tostring(h.link_to)
+    end
+    return ui.Span(" " .. h.name .. linked)
 end
